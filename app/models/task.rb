7ff93dc7e528debcec74a89e43b0771267033e81
kojimaizu <pyonpic@gmail.com>
2022-01-17 16:43:13 +0000

    kadai-tasklist2

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
     #contentのバリデーション
     validates :content, presence: true, length: { maximum: 255 }
     #statuのバリデーション
-    validates statu, presence: true, length: { maximum: 10 }
+    validates :status, presence: true, length: { maximum: 10 }
 end
--- db/migrate/20220117154839_add_statu_to_status.rb
+++
@@ -1,5 +0,0 @@
-class AddStatuToStatus < ActiveRecord::Migration[6.1]
-  def change
-    add_column :statuses, :statu, :string
-  end
-end
