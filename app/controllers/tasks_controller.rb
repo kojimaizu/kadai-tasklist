9c3412d6f371b2f9e33c90670e5f373341fbb25f
kojimaizu <pyonpic@gmail.com>
2022-01-30 13:11:38 +0000

    user hensyu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,16 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show]
+    before_action :correct_user, only: [:destroy]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.all 
+        if logged_in?
+          @tasks = current_user.tasks.order(id: :desc)
+      end
     end
     
     def show
+        @tasks = current_user.tasks.order(id: :desc)
     end
     
     def new
@@ -14,12 +18,13 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = "Task が正常に投稿されました"
             redirect_to @task
         else
+            @tasks = current_user.tasks.order(id: :desc)
             flash.now[:danger] = "Task が投稿されませんでした"
             render :new
         end    
@@ -43,7 +48,7 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = "Task は正常に削除されました"
-        redirect_to tasks_url
+        redirect_to root_url
     end 
     
     private
@@ -55,7 +60,13 @@ class TasksController < ApplicationController
     #セキュリティ強化
     def task_params
         params.require(:task).permit(:content, :status)
-    end    
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end
+    
 end
-
-#set_taskを消さないと二重呼び出し
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc)
   end  
 
   def create
