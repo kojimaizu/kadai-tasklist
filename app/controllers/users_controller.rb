ba1d5c2cdc74cf8a0cc5460d1c032cc13691c443
kojimaizu <pyonpic@gmail.com>
2022-02-06 04:41:29 +0000

    tasklist-hati8-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
-  def show
-    @user = User.find(params[:id])
-  end  
 
   def create
     @user = User.new(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
-  get 'users/new'
-  get 'users/create'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  delete "tasks/destroy"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks
+  resources :tasks, only: [:create, :new, :edit, :show, :update, :destroy]
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
-  
   get "signup", to: "users#new"
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create]
   
   
 end
